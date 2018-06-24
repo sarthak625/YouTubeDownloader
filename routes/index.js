cc8386d0fca99b3ac4d82da6bec3a688602fe044
Sarthak <sarthakn18@gmail.com>
2018-06-24 13:45:27 +0530

    Added checks on the url

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const http = require('http');
+const request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +10,28 @@ router.get('/', function(req, res, next) {
 
 router.post('/', (req, res, next)=>{
   console.log(req.body.url);
-  res.end("<h1>Submitted</h1>");
+  let url = req.body.url;
+  if (url.slice(0,8)!='https://'){
+    url = 'https://'+url;
+  }
+  console.log(url);
+  request(url,(err)=>{
+    if (err){
+      res.render('error',{'title':'Error','error':"The url you have entered is invalid"});
+    }
+    else{
+      if (url.includes('youtube') || url.includes('youtu.be')){
+        if (url ==='https://www.youtube.com' || url === 'https://youtube.com')
+        res.render('error',{'title':'Error','error':"Enter the video url"});  
+        else
+          res.render("download",{ title:' Download ', url: url});
+      }
+      else{
+        res.render('error',{'title':'Error','error':"The url you have entered is not a valid youtube url"});
+      }
+    }
+  })
+  
 });
 
 
